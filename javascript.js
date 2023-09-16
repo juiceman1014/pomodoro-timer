447e800706b1745aa1027e00089a206ee5b76dde
juiceman1014 <justintran1014@gmail.com>
2023-09-16 14:01:20 -0700

    creating reference variables for button, work/break seconds, and timer

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var start = document.getElementById("start");
+var rest = document.getElementById("stop");
+var stop = document.getElementById("reset");
+
+var wm = document.getElementById("w_minutes");
+var ws = document.getElementById("w_seconds");
+
+var bm = document.getElementById("b_minutes");
+var bs = document.getElementById("b_seconds");
+
+//store a reference to a timer variable
+var startTimer;
+
